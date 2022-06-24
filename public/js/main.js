7cd20252bde0aca6af5655842835fe46af43b510
Mahesh <mmiles3.1415@gmail.com>
2022-06-24 15:09:07 +0530

    nodejs first commit

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+const getDay = () => {
+    let weekdays = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+    let date = new Date()
+    return weekdays[date.getDay()]
+}
+
+const getMonth = () => {
+    let months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+    let date = new Date()
+    return months[date.getMonth()]
+}
+
+const getDate = () => {
+    let date = new Date()
+    return date.getDate()
+}
+
+const day = document.getElementById(day)
+const date = document.getElementById(today_date)
+
+day.innerHTML = getDay()
+date.innerHTML = `${getDate} ${getMonth}`
---
+++ src/app.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const bodyParser = require('body-parser')
+const requests = require('requests')
+
+const app = express()
+const port = process.env.PORT || 8000
+const static_path = path.join(__dirname, '../public')
+const templates_path = path.join(__dirname, '../templates/views')
+const partials_path = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', templates_path)
+hbs.registerPartials(partials_path)
+
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(express.static(static_path))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+// default object for weather data
+const default_obj = {
+    output_status : 'Enter the city name to see',
+    temp: 0,
+    weather_status: 'None',
+}
+
+app.get('/weather', (req, res) => {
+    res.render('weather', default_obj)
+})
+
+app.post('/weather', (req, res) => {
+    // console.log(req.body)
+
+    const cityname = req.body.cityname
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=e56df4762281a3442e899211379f93e3`
+    
+    requests(api)
+
+    .on('data', (chunk) => {
+        const objData = JSON.parse(chunk)
+        // console.log(objData)
+
+        if(objData.cod === 200) {
+            const l = Math.floor(Math.random()*objData.weather.length)
+            // console.log(l, objData.weather)
+
+            res.render('weather', {
+                output_status: '',
+                temp: Math.round((objData.main.temp-273.05)*100)/100,
+                weather_status: objData.weather[l].main,
+            })
+        }
+        else {
+            const revert = default_obj.output_status
+            default_obj.output_status = 'Please enter the correct city name'
+    
+            res.render('weather', default_obj)
+    
+            default_obj.output_status = revert
+        }
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('error')
+})
+
+app.listen(port, () => {
+    console.log(`listening to the port ${port}`)
+})
